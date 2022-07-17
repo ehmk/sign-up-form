f4751e4b914e58c1aca33cafbba34a42a5175e8c
Eric Hongmoungkhoune <ehmk90@gmail.com>
2022-07-17 18:37:05 -0400

    Finished password confirmation and form submission functionality

--- js/password.js
+++ js/password.js
@@ -1,20 +1,42 @@
 "use strict";
 
-let createAccountBtn = document.querySelector("#submit");
+const createAccountBtn = document.querySelector("#submit-btn");
 let password = document.querySelector("#password");
 let confirmPassword = document.querySelector("#confirm-password");
+const form = document.querySelector("#registration-form");
+const passwordError = document.querySelector("#password-error");
+
+const init = () => {
+  form.addEventListener("submit", handleSubmit);
+};
+
+window.addEventListener("load", init);
 
 const checkPassword = (password, confirmPassword) => {
-  if (password.value !== confirmPassword.value) {
-    console.log("Passwords must be the same.");
+  if (password.value === confirmPassword.value) {
+    return true;
+  } else {
+    password.style.border = "2px solid red";
+    confirmPassword.style.border = "2px solid red";
+    return false;
+  }
+};
+
+const handleSubmit = (event) => {
+  if (checkPassword(password, confirmPassword) === false) {
+    console.log("Registration failed");
+    passwordError.textContent = "Passwords must match";
+    event.preventDefault();
     return false;
   } else {
+    console.log("Registration successful");
+    document.getElementById("registration-form").submit();
+    document.getElementById("registration-form").reset();
+    password.style.border = "1px solid #e5e7eb";
+    confirmPassword.style.border = "1px solid #e5e7eb";
+    passwordError.textContent = "";
     return true;
   }
 };
 
-function hello() {
-  console.log("Hello");
-}
-
-createAccountBtn.addEventListener("click", () => checkPassword(password, confirmPassword));
+createAccountBtn.addEventListener("click", handleSubmit);
